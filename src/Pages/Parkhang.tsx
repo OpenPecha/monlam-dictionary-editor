8649846ee83f8360d571a3b42de7d8281ecf9ae2
Tenzin Tsering <tenzintsering630@gmail.com>
2025-01-01 10:43:59 +0530

    Completed more like component and two pages input field

--- src/Pages/Parkhang.tsx
+++ src/Pages/Parkhang.tsx
@@ -1,10 +1,40 @@
 import React from 'react'
+import logo from '../assets/logo.png'
+import home from '../assets/home.png'
+import arrow from '../assets/arrow.png'
+import { useNavigate } from 'react-router-dom'
 
 const Parkhang = () => {
+  const navigate = useNavigate();
+  function goTo(location: string) {
+    if (location === 'home') {
+      navigate('/');
+    } else {
+      console.log('Error while going to home page');
+    }
+  }
+
   return (
-    <div>
-      Parkhang
-    </div>
+    <>
+      <div className=' ml-16 mt-16'>
+        <img src={logo} className=' w-16 rounded-md'></img>
+        <p className=' text-4xl font-semibold'>སྨོན་ལམ་ཚིག་མཛོད་ཆེན་མོ་རྩོམ་སྒྲིག་མ་ལག</p>
+        <div className=' flex space-x-2 mt-6 bg-slate-300 p-2 rounded-md w-28 h-8'>
+          <img src={home} className=' w-4 h-4 cursor-pointer' onClick={() => goTo('home')}></img>
+          <img src={arrow} className=' w-2 h-5'></img>
+          <p className=' '>དཔར་ཁང་།</p>
+        </div>
+
+        <div className=' flex items-center border-b-2 border-black mt-9 pb-2 w-1/3'>
+          <label className=' text-2xl'>མིང་།</label>
+          <input className=' ml-14 outline-none mt-2 text-2xl w-96'></input>
+        </div>
+        <div className=' flex items-center border-b-2 border-black mt-2 pb-2 w-1/3'>
+          <label className=' text-2xl'>ཆགས་ཡུལ།</label>
+          <input className=' ml-6 outline-none mt-2 text-2xl w-96'></input>
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/Pages/Tsigsar.tsx
+++ src/Pages/Tsigsar.tsx
@@ -1,10 +1,52 @@
-import React from 'react'
+import logo from '../assets/logo.png'
+import home from '../assets/home.png'
+import arrow from '../assets/arrow.png'
+import { useNavigate } from 'react-router-dom'
+import StatusPending from '../Components/StatusPending'
+import StatusReviewed from '../Components/StatusReviewed'
+import plus from '../assets/plus.png'
 
 const Tsigsar = () => {
+  const navigate = useNavigate();
+
+  function goTo(location: string) {
+    if (location === 'home') {
+      navigate('/');
+    } else {
+      console.log('error while going back home page');
+    }
+  }
+
   return (
-    <div>
-      Tsigsar
-    </div>
+    <>
+      <div className=' ml-16 mt-16'>
+        <img src={logo} className=' w-16 rounded-md'></img>
+        <p className=' text-4xl font-semibold'>སྨོན་ལམ་ཚིག་མཛོད་ཆེན་མོ་རྩོམ་སྒྲིག་མ་ལག</p>
+        <div className=' flex space-x-2 mt-6 bg-slate-300 p-2 rounded-md w-28 h-8'>
+          <img src={home} className=' w-4 h-4 cursor-pointer' onClick={() => goTo('home')}></img>
+          <img src={arrow} className=' w-2 h-5'></img>
+          <p className=' '>དཔར་ཁང་།</p>
+        </div>
+
+        <div className=' mt-6'>
+          <StatusPending />
+        </div>
+
+        <div className=' flex items-center border-b-2 border-black mt-9 pb-2 w-1/3'>
+          <label className=' text-2xl'>མ་ཚིག</label>
+          <input className=' ml-4 outline-none mt-2 text-2xl w-96'></input>
+        </div>
+
+        <div className=' flex space-x-36 mt-6'>
+            <p className=' text-2xl'>མ་ཚིག་གསར་པ།</p>
+            <p className=' text-2xl'>རྒྱུན་སྤྱོད།</p>
+            <div className=' text-2xl'>འབྱུང་ཁུངས།</div>
+        </div>
+
+        <button className=' flex justify-center mt-8 w-28 rounded-md h-8 bg-gray-200 pb-5 gap-4 font-semibold transition-all duration-150 hover:opacity-80'>འགྲེལ་བཤད། <img src={plus} className=' w-4 mt-2'/></button>
+
+      </div>
+    </>
   )
 }
 
