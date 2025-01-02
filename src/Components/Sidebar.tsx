405980d4c7dab17fd1d0b1cc874fffb642d142c8
TenzDelek <tibetdelek@gmail.com>
2025-01-02 10:28:16 +0530

    Add Sidebar component and integrate Monlam font styling

---
+++ src/Components/Sidebar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className=' border w-96  font-monlam'>Sidebar</div>
+  )
+}
+
+export default Sidebar
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import Sidebar from "./Components/Sidebar.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App/>
-  </BrowserRouter>,
-)
+  <div className=" flex h-screen">
+  <Sidebar />
+  <App />
+  </div>
+    
+  </BrowserRouter>
+);
