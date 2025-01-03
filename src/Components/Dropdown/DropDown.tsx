f5872cdb18a136322fb899f0fd3af937bc4e6285
Tenzin Tsering <tenzintsering630@gmail.com>
2025-01-03 13:28:13 +0530

    text size changes

--- src/Components/Dropdown/DropDown.tsx
+++ src/Components/Dropdown/DropDown.tsx
@@ -10,7 +10,7 @@ const DropDown: React.FC<ChildProps> = ( {options, setSelect, setOpen} ) => {
   return (
     <div className=' absolute mt-2 border rounded-md w-56 overflow-y-auto h-32 bg-white'>
         {options.map((item,index) => (
-            <div className=' hover:bg-slate-200 p-2 h-8 cursor-pointer' onClick={() => {setSelect(item); setOpen((pre) => !pre)}}>{item}</div>
+            <div className=' flex items-center mt-1 hover:bg-slate-200 p-2 h-8 cursor-pointer text-2xl' onClick={() => {setSelect(item); setOpen((pre) => !pre)}}>{item}</div>
         ))}
     </div>
   )
