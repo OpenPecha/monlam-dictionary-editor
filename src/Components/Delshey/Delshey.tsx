00884cd802add5345f7143dfe22f45b55980db2f
TenzDelek <tibetdelek@gmail.com>
2025-01-06 11:00:55 +0530

    དཔར་ཁང་ཆ་ལག་ཚུན་ཆད་རྒྱ་སྐྱེད་བྱེད་པའི་འགྱུར་བ་མང་པོ་ཞིག་ལྷག་ཡོད།

--- src/Components/Delshey/Delshey.tsx
+++ src/Components/Delshey/Delshey.tsx
@@ -3,67 +3,57 @@ import DropDown from '../Dropdown/DropDown';
 import Chen from './Chen';
 import PechaPopup from './PechaPopup';
 import AuthorPopup from './AuthorPopup';
+import { Props } from '../../types/type';
+import { RxCross2,FaPlus } from '../../utils/Icons';
 
-interface Props {
-    popup: boolean[]
-    setPopup: React.Dispatch<React.SetStateAction<boolean[]>>;
-}
 
 const Delshey: React.FC<Props> = ( {popup, setPopup} ) => {
+    const dummydropdown1 = ['NOUN', 'VERB', 'PRONOUN'];
+    const dummydropdown2= ['DUMMY', 'DUMMY', 'DUMMY'];
+    const dummydropdown3=['RELIGIOUS', 'TEXTBOOK', 'SCIENCE'];
 
     const [newChen, setNewChen] = useState<boolean>(false);
     const [pechaPopup, setPechaPopup] = useState<boolean>(false);
     const [authorPopup, setAuthorPopup] = useState<boolean>(false);
-
-    // NOTE : The Drop down number is repective from left to right and from top to bottom
-
     const [dropDown1, setDropDown1] = useState<boolean>(false);
-    const [dropDownOption1, setDropDownOption1] = useState<string[]>(['NOUN', 'VERB', 'PRONOUN']);
     const [dropDownSelected1, setDropDownSelected1] = useState<string>('');
-    
     const [dropDown2, setDropDown2] = useState<boolean>(false);
-    const [dropDownOption2, setDropDownOption2] = useState<string[]>(['DUMMY', 'DUMMY', 'DUMMY']);
     const [dropDownSelected2, setDropDownSelected2] = useState<string>('');
-    
     const [dropDown3, setDropDown3] = useState<boolean>(false);
-    const [dropDownOption3, setDropDownOption3] = useState<string[]>(['DUMMY', 'DUMMY', 'DUMMY']);
     const [dropDownSelected3, setDropDownSelected3] = useState<string>('');
-    
     const [dropDown4, setDropDown4] = useState<boolean>(false);
-    const [dropDownOption4, setDropDownOption4] = useState<string[]>(['RELIGIOUS', 'TEXTBOOK', 'SCIENCE']);
     const [dropDownSelected4, setDropDownSelected4] = useState<string>('');
 
   return (
-    <>
-        <div className=' fixed inset-0 flex items-center justify-center bg-grey-800 bg-opacity-50 z-50'>
-            <div className=' bg-white border-black border-2 p-6 rounded-xl w-10/12 h-5/6'>
+        <div className='border font-monlam text-lg border-black fixed inset-0 flex items-center justify-center bg-grey-800 bg-opacity-50 z-50'>
+            <div className=' bg-white border-black border-2 p-6 rounded-xl w-fit h-5/6 overflow-scroll'>
                 <div className=' flex justify-between'>
-                    <p className=' font-monlam text-2xl font-bold'>འགྲེལ་བཤད།</p>
-                    <button onClick={() => setPopup([false,false,false])}>X</button>
+                    <p className='text-2xl font-bold'>འགྲེལ་བཤད།</p>
+                    <button onClick={() => setPopup([false,false,false])}><RxCross2/></button>
                 </div>
                 <div className=' flex'>
-                    <div className=' flex items-center border-b-2 border-black mt-3 pb-2 w-[660px]'>
-                        <label className=' text-1xl font-monlam w-16'>འགྲེལ་བ།</label>
-                        <input className=' ml-3 outline-none mt-0 text-2xl w-[510px]'></input>
+                    <div className=' flex items-center border-b-2 border-black mt-3 pb-2 '>
+                        <label>འགྲེལ་བ།</label>
+                        <input className=' ml-3 outline-none  w-[510px]'/>
                     </div>
-                    <div className=' flex items-center border-b-2 border-black mt-3 ml-4 pb-2 w-[260px]'>
-                        <label className=' text-1xl font-monlam w-64 cursor-pointer' onClick={() => setDropDown1(pre => !pre)}>བརྡ་སྤྲོད་དབྱེ་བའི་སྡེ་ཚན།</label>
-                        {dropDown1 ? <div className=' absolute z-50 mt-10'><DropDown options={dropDownOption1} setSelect={setDropDownSelected1} setOpen={setDropDown1} /> </div> : ''}
+                    <div className=' flex items-center border-b-2 border-black mt-3 ml-4 pb-2 '>
+                        <label className=' w-64 cursor-pointer' onClick={() => setDropDown1(pre => !pre)}>བརྡ་སྤྲོད་དབྱེ་བའི་སྡེ་ཚན།</label>
+                        {dropDown1 ? <div className=' absolute z-50 mt-10'><DropDown options={dummydropdown1} setSelect={setDropDownSelected1} setOpen={setDropDown1} /> </div> : ''}
                         <p className=' ml-[2px]'>{dropDownSelected1}</p>
                     </div>
-                    <div className=' flex items-center border-b-2 border-black mt-3 ml-4 pb-2 w-[210px]'>
-                        <label className=' text-1xl font-monlam w-64 cursor-pointer' onClick={() => setDropDown2(pre => !pre)}>སྤྱོད་སྒོ།</label>
-                        {dropDown2 ? <div className=' absolute z-50 mt-10'><DropDown options={dropDownOption2} setSelect={setDropDownSelected2} setOpen={setDropDown2} /> </div> : ''}
+                    <div className=' flex items-center border-b-2 border-black mt-3 ml-4 pb-2'>
+                        <label className=' w-64 cursor-pointer' onClick={() => setDropDown2(pre => !pre)}>སྤྱོད་སྒོ།</label>
+                        {dropDown2 ? <div className=' absolute z-50 mt-10'><DropDown options={dummydropdown2} setSelect={setDropDownSelected2} setOpen={setDropDown2} /> </div> : ''}
                         <p className=' ml-[2px]'>{dropDownSelected2}</p>
                     </div>
                 </div>
-                <div className=' flex'>
-                    <div className=' flex items-center border-b-2 border-black mt-3 pb-2 w-[700px]'>
-                        <label className=' text-1xl font-monlam w-36'>དཔེར་བརྗོད་ཚིག་གྲུབ།</label>
-                        <input className=' ml-2 outline-none mt-2 text-2xl w-[430px]'></input>
+                <div className='flex'>
+                    <div className=' flex items-center border-b-2 border-black mt-3 pb-2 '>
+                        <label>དཔེར་བརྗོད་ཚིག་གྲུབ།</label>
+                        <input className=' ml-2 outline-none mt-2 text-2xl w-[430px]'/>
                     </div>
-                    <div className=' flex items-center mt-3 ml-4 pb-2 w-[230px]'>
-                        <label className=' text-1xl font-monlam w-30'>པར་རིས་དགོས།</label>
+                    <div className=' flex items-center mt-3 ml-4 pb-2 '>
+                        <label>པར་རིས་དགོས།</label>
                         <div className=" ml-10 mt-0 flex items-center justify-center">
                             <label className="relative inline-block w-12 h-6 cursor-pointer">
                             <input type="checkbox" className="sr-only peer" />
@@ -72,20 +62,20 @@ const Delshey: React.FC<Props> = ( {popup, setPopup} ) => {
                             </label>
                         </div>
                     </div>
-                    <div className=' flex items-center border-b-2 border-black mt-3 ml-4 pb-2 w-[210px]'>
-                        <label className=' text-1xl font-monlam w-64 cursor-pointer' onClick={() => setDropDown3(pre => !pre)}>མིང་གི་རྣམ་གྲངས།</label>
-                        {dropDown3 ? <div className=' absolute z-50 mt-10'><DropDown options={dropDownOption3} setSelect={setDropDownSelected3} setOpen={setDropDown3} /> </div> : ''}
+                    <div className=' flex items-center border-b-2 border-black mt-3 ml-4 pb-2 w-[350px]'>
+                        <label className='cursor-pointer' onClick={() => setDropDown3(pre => !pre)}>མིང་གི་རྣམ་གྲངས།</label>
+                        {dropDown3 ? <div className=' absolute z-50 mt-10'><DropDown options={dummydropdown2} setSelect={setDropDownSelected3} setOpen={setDropDown3} /> </div> : ''}
                         <p className=' ml-[2px]'>{dropDownSelected3}</p>
                     </div>
                 </div>
                 <div className=' flex'>
-                    <div className=' flex items-center border-b-2 border-black mt-4 pb-2 w-[700px]'>
-                        <label className=' text-1xl font-monlam w-12'>མཆན།</label>
-                        <p className=' ml-5 cursor-pointer' onClick={() => setNewChen(true)}>Extra selection</p>
+                    <div className=' flex items-center justify-between border-b-2 border-black mt-4 pb-2 w-[700px]'>
+                        <label>མཆན།</label>
+                        <p className=' ml-5 cursor-pointer' onClick={() => setNewChen(true)}><FaPlus/></p>
                     </div>
-                    <div className=' flex items-center border-b-2 border-black mt-3 ml-4 pb-2 w-[460px]'>
-                        <label className=' text-1xl font-monlam w-64 cursor-pointer' onClick={() => setDropDown4(pre => !pre)}>བརྡ་ཆད་དབྱེ་བའི་སྡེ་ཚན།</label>
-                        {dropDown4 ? <div className=' absolute z-50 mt-10'><DropDown options={dropDownOption4} setSelect={setDropDownSelected4} setOpen={setDropDown4} /> </div> : ''}
+                    <div className=' flex items-center border-b-2 border-black mt-3 ml-4 pb-2 w-[410px]'>
+                        <label className=' cursor-pointer' onClick={() => setDropDown4(pre => !pre)}>བརྡ་ཆད་དབྱེ་བའི་སྡེ་ཚན།</label>
+                        {dropDown4 ? <div className=' absolute z-50 mt-10'><DropDown options={dummydropdown3} setSelect={setDropDownSelected4} setOpen={setDropDown4} /> </div> : ''}
                         <p className=' ml-[2px]'>{dropDownSelected4}</p>
                     </div>
                 </div>
@@ -93,9 +83,8 @@ const Delshey: React.FC<Props> = ( {popup, setPopup} ) => {
             </div>
             {pechaPopup ? <PechaPopup /> : ''}
             {authorPopup ? <AuthorPopup /> : ''}
-            <button className=' fixed bottom-[100px] right-[180px] bg-slate-400 pl-7 pr-7 pt-2 pb-2 rounded-md hover:opacity-80 active:opacity-50 font-monlam'>ཉར་ཚགས།</button>
+            <button className=' fixed bottom-[100px] right-[180px] bg-slate-400 pl-7 pr-7 pt-2 pb-2 rounded-md hover:opacity-80 active:opacity-50 '>ཉར་ཚགས།</button>
         </div>
-    </>
   )
 }
 
--- src/Pages/Parkhang.tsx
+++ src/Pages/Parkhang.tsx
@@ -1,44 +1,110 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import logo from '../assets/logo.png'
 import { useForm, SubmitHandler } from 'react-hook-form'
 import Breadcrumb from '../Components/Breadcrumb'
 import Submits from '../Components/Submit'
 import { InputParchang } from '../types/type'
+import SuccessMessage from '../Components/SuccessMessage'
+import { useNavigate } from 'react-router-dom'
 
 
 
 const Parkhang = () => {
+  const navigate = useNavigate()
+  const [showSuccess, setShowSuccess] = useState<boolean>(false)
+  
   const {
     register,
     handleSubmit,
     reset,
-    formState: {errors},
+    formState: { errors, isSubmitting }
   } = useForm<InputParchang>()
 
-  const onSubmit: SubmitHandler<InputParchang> = (data) => {
-    console.log(data);
-    reset();
-  }
-  return (
-    <>
-      <div className=' text-lg font-monlam ml-16 mt-16'>
-      <img src={logo} className=' w-16 rounded-md'></img>
-      <p className=' text-xl font-semibold mt-2'>སྨོན་ལམ་ཚིག་མཛོད་ཆེན་མོ་རྩོམ་སྒྲིག་མ་ལག</p>
-        <Breadcrumb name="དཔར་ཁང་།"/>
+  useEffect(() => {
+    let navigationTimer:any
+    if (showSuccess) {
+      navigationTimer = setTimeout(() => {
+        navigate('/')
+      }, 3000)
+    }
+    
+    return () => {
+      if (navigationTimer) {
+        clearTimeout(navigationTimer)
+      }
+    }
+  }, [showSuccess, navigate])
 
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className=' flex items-center border-b-2 border-black mt-9 pb-2 w-fit'>
-            <label>མིང་།</label>
-            <input className=' ml-14 outline-none mt-2 text-lg' {...register('name', { required: true })}></input>
+  const onSubmit: SubmitHandler<InputParchang> = async (data) => {
+    try {
+      console.log(data)
+      reset()
+      setShowSuccess(true)
+      
+    } catch (error) {
+      console.error('Form submission error:', error)
+      setShowSuccess(false)
+    }
+  }
+
+  return (
+      <div className="font-monlam p-16">
+        <img 
+          src={logo} 
+          alt="Logo" 
+          className="w-16 rounded-md"
+        />
+        <p className="text-xl font-semibold mt-2">
+          སྨོན་ལམ་ཚིག་མཛོད་ཆེན་མོ་རྩོམ་སྒྲིག་མ་ལག
+        </p>
+        
+        <Breadcrumb name="དཔར་ཁང་།" />
+
+        <form 
+          onSubmit={handleSubmit(onSubmit)}
+          className="mt-9 space-y-4"
+        >
+          <div className=' flex items-center '>
+          <div className="flex items-center border-b-2 border-black pb-2 w-fit">
+            <label htmlFor="name">མིང་།</label>
+            <input
+              id="name"
+              className=" ml-2 outline-none text-lg w-64"
+              {...register('name', { 
+                required: 'Name is required' 
+              })}
+            />
+          
           </div>
-          <div className=' flex items-center border-b-2 border-black mt-4 pb-2 w-fit'>
-            <label>ཆགས་ཡུལ།</label>
-            <input className=' ml-6 outline-none mt-2 text-lg' {...register('chakyul', {required: true})}></input>
+          {errors.name && (
+              <span className="text-red-500 font-inter text-sm ml-2">
+                *{errors.name.message}
+              </span>
+            )}
           </div>
-          <Submits/>
+
+          <div className='flex items-center'>
+          <div className="flex items-center border-b-2 border-black pb-2 w-fit">
+            <label htmlFor="chakyul">ཆགས་ཡུལ།</label>
+            <input
+              id="chakyul"
+              className="ml-4 outline-none text-lg"
+              {...register('chakyul', { 
+                required: 'Chakyul is required' 
+              })}
+            />
+           
+          </div>
+          {errors.chakyul && (
+              <span className="text-red-500 font-inter text-sm ml-2">
+                *{errors.chakyul.message}
+              </span>
+            )}
+          </div>
+          <Submits disabled={isSubmitting} />
         </form>
+        {showSuccess && <SuccessMessage />}
       </div>
-    </>
   )
 }
 
